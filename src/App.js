28fd39220fa91282f14ea032845ea5d90ad49332
Esh shatabdi <eshshatabdi@gmail.com>
2022-03-18 14:34:34 -0400

    component commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,55 @@ import { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
+      <ToDoList></ToDoList>
 
-      )
+
     </div>
   );
 }
-function todolist() {
-  const [todo, setTodo] = useState([]);
+function ToDoList() {
+  const [todos, setTodo] = useState([]);
+
   useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(data => setTodo(data))
 
 
   }, [])
   return (
 
     <div>
+      <h2>To Do of React</h2>
+      {
+        todos.map(todo => <Countries userId={todo.userId} title={todo.title}
+          completed={todo.completed ? 'done' : 'not completed'}></Countries>)
+
+      }
+
 
 
 
     </div>
   )
+}
+
+function Countries(props) {
+  console.log(props)
+
+  return (
+    <div style={{
+      backgroundColor: 'lightblue',
+
+      margin: '10px',
+      padding: '20px',
+    }}>
+      <h2>UserID:{props.userId} </h2>
+      <p>Title:{props.title} </p>
+      <p>Completed:{props.completed}</p>
+    </div>
+  )
+
 }
 
 export default App;
